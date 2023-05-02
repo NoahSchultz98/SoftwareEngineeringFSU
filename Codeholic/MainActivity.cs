075822593deb02ac4c482c197cfa1a5c55732839
Noah Schultz <nschult1@student.fitchburgstate.edu>
2023-05-02 18:26:27 -0400

    Merge?
    I think this is going to break things

--- Codeholic/MainActivity.cs
+++ Codeholic/MainActivity.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.Res;
@@ -83,3 +84,162 @@ namespace Codeholic
     }
 }
 
+=======
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomNavigation;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using static Codeholic.Resources.Extensions;
+using static Android.Bluetooth.BluetoothClass;
+using System.Net;
+using Codeholic.Resources;
+using Codeholic.SQL;
+using System.Text.Json;
+
+namespace Codeholic
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        TextView textMessage;
+
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            //ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+
+            Button pickFile = FindViewById<Button>(Resource.Id.btnPickFile);
+            pickFile.Click += PickFile_Click;
+#if DEBUG
+            // During development, we can trust all certificates, including ASP.NET developer certificates
+            // DO NOT ENABLE THIS IN RELEASE BUILDS
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (_, __, ___, ____) => true;
+#endif
+            TestConn();
+            // for test:
+            //getID();
+        }
+
+        private async void TestConn()
+        {
+            Codeholic.Resources.WebResponse response = await DatabaseConnection.Query("SELECT * FROM user WHERE userID = 1");
+
+
+
+            SQL.User theUserFound = JsonSerializer.Deserialize<User>(response.data);
+            Toast.MakeText(Android.App.Application.Context, "Greetings from " + theUserFound.username, ToastLength.Long).Show();
+            //Toast.MakeText(Android.App.Application.Context, response.data, ToastLength.Long).Show();
+            //DatabaseConnection.GetUserInfo();
+        }
+
+        private void PickFile_Click(object sender, EventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await PickAndShow(PickOptions.Images);
+            });
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                // when we click the home, dash, notifications button, it just changes the text message. okay. simple enough. we can make mine open up the plugin manager.
+                case Resource.Id.navigation_home:
+                    textMessage.SetText(Resource.String.title_home);
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    textMessage.SetText(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    textMessage.Text = "Plugin Manager"; // just a hack for now
+                    // code to begin new activity
+                    Intent nextActivity = new Intent(this, typeof(Resources.PluginManagementSystemActivity));
+                    StartActivity(nextActivity);
+                    return true;
+            }
+            return false;
+        }
+
+        /*
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            FilePicker.PickAsync();
+
+        }
+        */
+        /*
+        public void OnClick(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.btnPickFile:
+                    MainThread.BeginInvokeOnMainThread(async () =>
+                    {
+                        await PickAndShow(PickOptions.Images);
+                    });
+                    break;
+            }
+        }
+
+        public void ButtonClicked(View view)
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await PickAndShow(PickOptions.Images);
+            });
+        }
+
+        
+    }
+        */
+        async Task<FileResult> PickAndShow(PickOptions options)
+        {
+            try
+            {
+                var result = await FilePicker.PickAsync(options);
+                if (result != null)
+                {
+                    textMessage.Text = $"File Name: {result.FileName}";
+                    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var stream = await result.OpenReadAsync();
+                        //FileImage.Source = ImageSource.FromStream(() => stream);
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // The user canceled or something went wrong
+                _ = ex;
+            }
+
+            return null;
+        }
+    }
+}
+
+>>>>>>> 0ab5a17... plugin manager and plugin uploader both functional (pull data from DB and populate fields, update DB when fields are changed). added DatabaseConnection.cs for sending SQL queries.
--- Codeholic/obj/Debug/130/android/src/xamarin/essentials/fileProvider.java
+++ Codeholic/obj/Debug/130/android/src/xamarin/essentials/fileProvider.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package xamarin.essentials;
 
 
@@ -37,3 +38,53 @@ public class fileProvider
 			refList.clear ();
 	}
 }
+=======
+package xamarin.essentials;
+
+
+public class fileProvider
+	extends androidx.core.content.FileProvider
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Xamarin.Essentials.FileProvider, Xamarin.Essentials", fileProvider.class, __md_methods);
+	}
+
+
+	public fileProvider ()
+	{
+		super ();
+		if (getClass () == fileProvider.class) {
+			mono.android.TypeManager.Activate ("Xamarin.Essentials.FileProvider, Xamarin.Essentials", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public fileProvider (int p0)
+	{
+		super (p0);
+		if (getClass () == fileProvider.class) {
+			mono.android.TypeManager.Activate ("Xamarin.Essentials.FileProvider, Xamarin.Essentials", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
+>>>>>>> 0ab5a17... plugin manager and plugin uploader both functional (pull data from DB and populate fields, update DB when fields are changed). added DatabaseConnection.cs for sending SQL queries.
